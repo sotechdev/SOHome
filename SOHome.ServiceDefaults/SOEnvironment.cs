0c3c41c5a799b73307216756e952edacedd6de2a
Osni Pezzini Junior <osni.pezzini@gmail.com>
2025-06-14 18:30:05 -0300

    Criado projeto de backend .NET

---
+++ SOHome.ServiceDefaults/SOEnvironment.cs
@@ -0,0 +1,7 @@
+﻿namespace SOHome.ServiceDefaults;
+
+public sealed class SOEnvironment
+{
+    public static readonly bool IsDebug =
+        Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development" || Environment.GetEnvironmentVariable("SODEBUG") == "1";
+}
